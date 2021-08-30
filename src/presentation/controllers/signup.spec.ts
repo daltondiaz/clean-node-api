7c0a2f6ba825e97a0bd7ecf8b9fa67bba28ae51e
daltondiaz <dalton.meira@simova.com.br>
2021-08-29 22:10:42 -0300

    test: ensure SignUpController return 400 if no password confirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,3 +48,19 @@ describe('SignUp Controller', () => {
     expect(httpResponde.body).toEqual(new MissingParamError('password'))
   })
 })
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponde = sut.handle(httpRequest)
+    expect(httpResponde.statusCode).toBe(400)
+    expect(httpResponde.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+})
