dd1688d0e5cf13d7e4feb50bd9ddee470020374a
daltondiaz <dalton.meira@simova.com.br>
2021-11-15 19:02:34 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -26,4 +26,11 @@ describe('Bcrypt Adapter', () => {
      const hash = await sut.encrypt('any_value')
      expect(hash).toBe('hash')
    })
+
+   test('Should throw if bcrypt throws', async () => {
+     const sut = makeSut()
+     jest.spyOn(bcrypt, 'hash').mockImplementation(() => Promise.resolve(new Error()))
+     const promise = sut.encrypt('any_value')
+     expect(promise).rejects.toThrow()
+   })
 })
