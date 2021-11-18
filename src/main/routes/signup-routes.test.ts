266082058fd8fd7f635191ae3f190bafb1f395b8
daltondiaz <dalton.meira@simova.com.br>
2021-11-17 23:15:37 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,16 +1,30 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helpers'
 import app from '../config/app'
 
 describe('SignUp Routes', () => {
-   test('Should return an account on succes ', async () => {
-     await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Dalton',
-        email: 'dalton.dias@ymail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200)
-   })
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getConnection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  test('Should return an account on succes ', async () => {
+    await request(app)
+    .post('/api/signup')
+    .send({
+      name: 'Dalton',
+      email: 'dalton.dias@ymail.com',
+      password: '123',
+      passwordConfirmation: '123'
+    })
+    .expect(200)
+  })
 })
