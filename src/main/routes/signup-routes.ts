58013568b46a5d69d2c08ca790947c6094a256be
daltondiaz <d_tecnologia@yahoo.com.br>
2021-12-19 16:21:08 -0300

    feat: add LoginValidationFactory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { makeSignUpController } from '../factories/signup'
+import { makeSignUpController } from '../factories/signup/signup'
 import { adaptRoute } from '../adapters/express-routes-adapter'
 
 export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
 }
