a9494919c0559320c8472f571c83015512b864d6
daltondiaz <d_tecnologia@yahoo.com.br>
2021-12-25 23:47:16 -0300

    refactor: rename Encrypter to Hasher

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
-  async hash(): Promise<string> {
-    return new Promise(resolve => resolve('hash'))
-  } 
+  async hash (): Promise<string> {
+    return await new Promise(resolve => resolve('hash'))
+  }
 }))
 
 const salt = 12
@@ -14,23 +15,24 @@ const makeSut = (): BcryptAdapter => {
 }
 
 describe('Bcrypt Adapter', () => {
-   test('Should call bcrypt with correct values', async () => {
-     const sut = makeSut()
-     const hasSpy = jest.spyOn(bcrypt, 'hash')
-     await sut.encrypt('any_value')
-     expect(hasSpy).toHaveBeenCalledWith('any_value', salt)
-   })
+  test('Should call bcrypt with correct values', async () => {
+    const sut = makeSut()
+    const hasSpy = jest.spyOn(bcrypt, 'hash')
+    await sut.hash('any_value')
+    expect(hasSpy).toHaveBeenCalledWith('any_value', salt)
+  })
 
-   test('Should return a hash on success', async () => {
-     const sut = makeSut()
-     const hash = await sut.encrypt('any_value')
-     expect(hash).toBe('hash')
-   })
+  test('Should return a hash on success', async () => {
+    const sut = makeSut()
+    const hash = await sut.hash('any_value')
+    expect(hash).toBe('hash')
+  })
 
-   test('Should throw if bcrypt throws', async () => {
-     const sut = makeSut()
-     jest.spyOn(bcrypt, 'hash').mockImplementation(() => Promise.resolve(new Error()))
-     const promise = sut.encrypt('any_value')
-     expect(promise).rejects.toThrow()
-   })
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementation(async () => await Promise.resolve(new Error()))
+    const promise = sut.hash('any_value')
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    expect(promise).rejects.toThrow()
+  })
 })
