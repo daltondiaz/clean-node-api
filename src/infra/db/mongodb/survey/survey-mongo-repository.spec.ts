ff78448367b8320abdafff56b057f610752762bb
daltondiaz <d_tecnologia@yahoo.com.br>
2022-02-07 23:17:18 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -41,8 +41,29 @@ describe('Survey Mongo Repository', () => {
       expect(survey.question).toBe('any_question')
     })
   })
-  describe('add()', () => {
-    test('', () => {
+  describe('loadAll()', () => {
+    test('Should loadAll surveys on success', async () => {
+      await surveyCollection.insertMany([{
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      },
+      {
+        question: 'other_question',
+        answers: [{
+          image: 'other_image',
+          answer: 'other_answer'
+        }],
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
     })
   })
 })
