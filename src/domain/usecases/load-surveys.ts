fc0b84a60701f5edd809686f10789c1544ec324c
daltondiaz <d_tecnologia@yahoo.com.br>
2022-02-27 15:38:15 -0300

    feat: add SaveSurveyResult usecase

--- src/domain/usecases/load-surveys.ts
+++ src/domain/usecases/load-surveys.ts
@@ -1,6 +1,5 @@
 import { SurveyModel } from '@/domain/models/survey'
 
 export interface LoadSurveys {
-  // eslint-disable-next-line @typescript-eslint/method-signature-style
   load(): Promise<SurveyModel[]>
 }
