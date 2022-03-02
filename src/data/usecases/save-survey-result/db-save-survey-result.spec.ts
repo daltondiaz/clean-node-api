4b606533ca731da7db813dcf56f605072dcba092
daltondiaz <d_tecnologia@yahoo.com.br>
2022-03-01 21:51:40 -0300

    test: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct value

---
+++ src/data/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -0,0 +1,56 @@
+import { DbSaveSurveyResult } from './db-save-survey-result'
+import MockDate from 'mockdate'
+import { SurveyResultModel, SaveSurveyResultModel, SaveSurveyResultRepository } from './db-save-survey-result-protocols'
+
+const makeFakeSurveyResultData = (): SaveSurveyResultModel => ({
+
+  accountId: 'any_account_id',
+  surveyId: 'any_survey_id',
+  answers: 'any_answer',
+  date: new Date()
+})
+
+const makeFakeSurveyResult = (): SurveyResultModel => Object.assign({}, makeFakeSurveyResultData(), {
+  id: 'any_id'
+})
+
+type SutTypes = {
+  sut: DbSaveSurveyResult
+  saveSurveyResultRepositoryStub: SaveSurveyResultRepository
+}
+
+const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
+  class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
+    async save (surveyData: SaveSurveyResultModel): Promise<SurveyResultModel> {
+      return await new Promise(resolve => resolve(makeFakeSurveyResult()))
+    }
+  }
+  return new SaveSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const saveSurveyResultRepositoryStub = makeSaveSurveyResultRepository()
+  const sut = new DbSaveSurveyResult(saveSurveyResultRepositoryStub)
+  return {
+    sut,
+    saveSurveyResultRepositoryStub
+  }
+}
+
+describe('DbSaveSurveyResult UseCase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
+  test('Should call SaveSurveyRepository with correct values', async () => {
+    const { sut, saveSurveyResultRepositoryStub } = makeSut()
+    const saveSpy = jest.spyOn(saveSurveyResultRepositoryStub, 'save')
+    const surveyResultData = makeFakeSurveyResultData()
+    await sut.save(surveyResultData)
+    expect(saveSpy).toHaveBeenCalledWith(surveyResultData)
+  })
+})
