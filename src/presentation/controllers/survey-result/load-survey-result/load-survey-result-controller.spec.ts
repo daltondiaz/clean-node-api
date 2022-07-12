a4407df0a176ed2f041b1f4743253bc913061e6f
daltondiaz <d_tecnologia@yahoo.com.br>
2022-07-11 22:42:15 -0300

    test: ensure LoadSurveyResultController returns 403 if and invalid surveyId is provided

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,8 +1,10 @@
+import { InvalidParamError } from '@/presentation/errors'
+import { forbideen } from '@/presentation/helpers/http/http-helper'
 import { mockLoadSurveyById } from '@/presentation/test'
 import { LoadSurveyResultController } from './load-survey-result-controller'
 import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
 
-const mockFakeRequest = (): HttpRequest => ({
+const makeFakeRequest = (): HttpRequest => ({
   params: {
     surveyId: 'any_id'
   }
@@ -22,7 +24,14 @@ describe('LoadSurveyResult Controller', () => {
   test('Should call LoadSurveyById with correct value', async () => {
     const { sut, loadSurveyByIdStub } = makeSut()
     const loadByIdSpy = jest.spyOn(loadSurveyByIdStub, 'loadById')
-    await sut.handle(mockFakeRequest())
+    await sut.handle(makeFakeRequest())
     expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return 403 if LoadSurveyById returns null', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut()
+    jest.spyOn(loadSurveyByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbideen(new InvalidParamError('surveyId')))
   })
 })
