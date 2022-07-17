ad2b377ddd8c6b4cd87dbf3414cc4b2baa02cb85
daltondiaz <d_tecnologia@yahoo.com.br>
2022-07-17 00:05:36 -0300

    docs: add load survey result path

--- requirements/load-survey-result .md
+++ requirements/load-survey-result .md
@@ -4,10 +4,10 @@
 
 1. ✅ Recebe uma requisição do tipo **GET** na rota **/api/surveys/{survey_id}/results**
 2. ✅ Valida se a requisição foi feita por um **usuário**
-3. ⛔️ Retorna **200** com os dados do resultdo da enquete
+3. ✅ Retorna **200** com os dados do resultdo da enquete
 
 > ## Exceções:
 
-1. ⛔️ Retorna erro **404** se a API não existir
+1. ✅ Retorna erro **404** se a API não existir
 2. ✅ Retorna erro **403** se não for um usuário
 3. ✅ Retorna erro **500** se der error ao tentar listar as enquetes
--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -43,5 +43,41 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    tags: ['Survey'],
+    summary: 'API to get the result from survey',
+    security: [{
+      apiKeyAuth: []
+    }],
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
