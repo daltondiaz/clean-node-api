6205825a33b83e6cb90ded4f51926d4d06fca2fb
daltondiaz <d_tecnologia@yahoo.com.br>
2022-07-16 23:49:01 -0300

    test: ensure load survey routes returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -67,7 +67,7 @@ describe('Survey Routes', () => {
       })
       const { insertedId: id } = res
       await request(app)
-        .put(`/api/surveys/${id.toHexString()}/results`)
+        .get(`/api/surveys/${id.toHexString()}/results`)
         .set('x-access-token', accesToken)
         .send({
           answer: 'Answer 1'
@@ -81,6 +81,28 @@ describe('Survey Routes', () => {
       await request(app)
         .get('/api/surveys/any_id/results')
         .expect(403)
+    })
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accesToken = await makeAccessToken()
+      const res = await surveysCollections.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      const { insertedId: id } = res
+      await request(app)
+        .put(`/api/surveys/${id.toHexString()}/results`)
+        .set('x-access-token', accesToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
     })
   })
 })
