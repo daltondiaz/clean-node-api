83da1240e6154a391dd5c85a0277974ccc48e7d7
daltondiaz <d_tecnologia@yahoo.com.br>
2022-08-21 16:44:20 -0300

    feat: add name to authentication response

--- src/domain/usecases/account/authentication.ts
+++ src/domain/usecases/account/authentication.ts
@@ -1,8 +1,10 @@
+import { AuthenticationModel } from '@/domain/models/account'
+
 export type AuthenticationParams = {
   email: string
   password: string
 }
 
 export type Authentication = {
-  auth: (authentication: AuthenticationParams) => Promise<string>
+  auth: (authentication: AuthenticationParams) => Promise<AuthenticationModel>
 }
