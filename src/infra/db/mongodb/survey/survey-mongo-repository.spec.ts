d9558bd211aba5479afe4f67bf058a2eee976bb1
daltondiaz <d_tecnologia@yahoo.com.br>
2022-08-22 23:10:35 -0300

    refactor: use mockAddSurveyParams instead of manual data

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -1,6 +1,7 @@
 import { MongoHelper } from '../helpers/mongo-helpers'
 import { SurveyMongoRepository } from './survey-mongo-repository'
 import { Collection } from 'mongodb'
+import { mockAddSurveyParams } from '@/domain/test'
 
 let surveyCollection: Collection
 
@@ -25,46 +26,21 @@ describe('Survey Mongo Repository', () => {
   describe('add()', () => {
     test('Should add a survey on success', async () => {
       const sut = makeSut()
-      await sut.add({
-        question: 'any_question',
-        answers: [{
-          image: 'any_image',
-          answer: 'any_answer'
-        },
-        {
-          answer: 'other_answer'
-        }],
-        date: new Date()
-      })
-      const survey = await surveyCollection.findOne({ question: 'any_question' })
-      expect(survey).toBeTruthy()
-      expect(survey.question).toBe('any_question')
+      await sut.add(mockAddSurveyParams())
+      const count = await surveyCollection.countDocuments()
+      expect(count).toBe(1)
     })
   })
   describe('loadAll()', () => {
     test('Should load all surveys on success', async () => {
-      await surveyCollection.insertMany([{
-        question: 'any_question',
-        answers: [{
-          image: 'any_image',
-          answer: 'any_answer'
-        }],
-        date: new Date()
-      },
-      {
-        question: 'other_question',
-        answers: [{
-          image: 'other_image',
-          answer: 'other_answer'
-        }],
-        date: new Date()
-      }])
+      const addSurveysModel = [mockAddSurveyParams(), mockAddSurveyParams()]
+      await surveyCollection.insertMany(addSurveysModel)
       const sut = makeSut()
       const surveys = await sut.loadAll()
       expect(surveys.length).toBe(2)
       expect(surveys[0].id).toBeTruthy()
-      expect(surveys[0].question).toBe('any_question')
-      expect(surveys[1].question).toBe('other_question')
+      expect(surveys[0].question).toBe(addSurveysModel[0].question)
+      expect(surveys[1].question).toBe(addSurveysModel[1].question)
     })
 
     test('Should load empty list', async () => {
